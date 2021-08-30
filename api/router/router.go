789b6ffd2c71d9927e4189dd0e051d5cdb5ded1f
陳 念歆 <ch1342213422@gmail.com>
2021-08-30 22:37:08 +0800

    router

--- api/router/router.go
+++ api/router/router.go
@@ -27,5 +27,184 @@ func InitRouter() *gin.Engine {
 
 	router.DELETE("/DeleteAuth", Auth_d)
 
+	//Countersign
+	router.GET("/SelectCountersign")
+
+	router.POST("/UploadCountersign")
+
+	router.PUT("/UpdateCountersign")
+
+	router.DELETE("/DeleteCountersign")
+
+	//Customer
+	router.GET("/SelectCustomer")
+
+	router.POST("/UploadCustomer")
+
+	router.PUT("/UpdateCustomer")
+
+	router.DELETE("/DeleteCustomer")
+
+	//Countersign
+	router.GET("/SelectCountersign")
+
+	router.POST("/UploadCountersign")
+
+	router.PUT("/UpdateCountersign")
+
+	router.DELETE("/DeleteCountersign")
+
+	//Customer_demand
+	router.GET("/SelectCustomerDemand")
+
+	router.POST("/UploadCustomerDemand")
+
+	router.PUT("/UpdateCustomerDemand")
+
+	router.DELETE("/DeleteCustomerDemand")
+
+	//Department
+	router.GET("/SelectDepartment")
+
+	router.POST("/UploadDepartment")
+
+	router.PUT("/UpdateDepartment")
+
+	router.DELETE("/DeleteDepartment")
+
+	//Files
+	router.GET("/SelectFiles")
+
+	router.POST("/UploadFiles")
+
+	router.PUT("/UpdateFiles")
+
+	router.DELETE("/DeleteFiles")
+
+	//Interview
+	router.GET("/SelectInterview")
+
+	router.POST("/UploadInterview")
+
+	router.PUT("/UpdateInterview")
+
+	router.DELETE("/DeleteInterview")
+
+	//Jid_demand
+	router.GET("/SelectJidDemand")
+
+	router.POST("/UploadJidDemand")
+
+	router.PUT("/UpdateJidDemand")
+
+	router.DELETE("/DeleteJidDemand")
+
+	//Log
+	router.GET("/SelectLog")
+
+	router.POST("/UploadLog")
+
+	router.PUT("/UpdateLog")
+
+	router.DELETE("/DeleteLog")
+
+	//Machine_type
+	router.GET("/SelectMachineType")
+
+	router.POST("/UploadMachineType")
+
+	router.PUT("/UpdateMachineType")
+
+	router.DELETE("/DeleteMachineType")
+
+	//Machine_work_place
+	router.GET("/SelectMachineWorkPlace")
+
+	router.POST("/UploadMachineWorkPlace")
+
+	router.PUT("/UpdateMachineWorkPlace")
+
+	router.DELETE("/DeleteMachineWorkPlace")
+
+	//Manufactrue_order
+	router.GET("/SelectManufactrueOrder")
+
+	router.POST("/UploadManufactrueOrder")
+
+	router.PUT("/UpdateManufactrueOrder")
+
+	router.DELETE("/DeleteManufactrueOrder")
+
+	//Meeting
+	router.GET("/SelectMeeting")
+
+	router.POST("/UploadMeeting")
+
+	router.PUT("/UpdateMeeting")
+
+	router.DELETE("/DeleteMeeting")
+
+	//NoticeTime
+	router.GET("/SelectNoticeTime")
+
+	router.POST("/UploadNoticeTime")
+
+	router.PUT("/UpdateNoticeTime")
+
+	router.DELETE("/DeleteNoticeTime")
+
+	//Project
+	router.GET("/SelectProject")
+
+	router.POST("/UploadProject")
+
+	router.PUT("/UpdateProjecte")
+
+	router.DELETE("/DeleteProject")
+
+	//Sysuser
+	router.GET("/SelectSysuser")
+
+	router.POST("/UploadSysuser")
+
+	router.PUT("/UpdateSysuser")
+
+	router.DELETE("/DeleteSysuser")
+
+	//Task
+	router.GET("/SelectTask")
+
+	router.POST("/UploadTask")
+
+	router.PUT("/UpdateTask")
+
+	router.DELETE("/DeleteTask")
+
+	//Work_item
+	router.GET("/SelectWorkItem")
+
+	router.POST("/UploadWorkItem")
+
+	router.PUT("/UpdateWorkItem")
+
+	router.DELETE("/DeleteWorkItem")
+
+	//Work_set
+	router.GET("/SelectWorkSet")
+
+	router.POST("/UploadWorkSet")
+
+	router.PUT("/UpdateWorkSet")
+
+	router.DELETE("/DeleteWorkSet")
+
+	//Auth
+	router.GET("/SelectAuth")
+
+	router.POST("/UploadAuth")
+
+	router.PUT("/UpdateAuth")
+
+	router.DELETE("/DeleteAuth")
 	return router
 }
